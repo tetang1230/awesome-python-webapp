b3a44a26c6736f1d8775450a8e1d53680de0bb8a
tetang1230 <tetang1230@gmail.com>
2017-05-12 23:22:39 +0800

    更改mysql配置

--- schema.sql
+++ schema.sql
@@ -6,7 +6,7 @@ create database awesome;
 
 use awesome;
 
-grant select, insert, update, delete on awesome.* to 'www-data'@'localhost' identified by 'www-data';
+-- grant select, insert, update, delete on awesome.* to 'www-data'@'localhost' identified by 'www-data';
 
 create table users (
     `id` varchar(50) not null,
@@ -49,4 +49,4 @@ create table comments (
 -- email / password:
 -- admin@example.com / password
 
-insert into users (`id`, `email`, `password`, `admin`, `name`, `created_at`) values ('0010018336417540987fff4508f43fbaed718e263442526000', 'admin@example.com', '5f4dcc3b5aa765d61d8327deb882cf99', 1, 'Administrator', 1402909113.628);
+insert into users (`id`, `email`, `password`, `admin`, `name`, `created_at`) values ('0010018336417540987fff4508f43fbaed718e263442526000', 'admin@example.com', 'e10adc3949ba59abbe56e057f20f883e', 1, 'Administrator', 1402909113.628);
