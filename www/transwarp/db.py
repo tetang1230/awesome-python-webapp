31137bfdd2496764a52f8b2c8d378ba709ea6891
tetang1230 <tetang1230@gmail.com>
2017-05-12 23:54:08 +0800

    update db

--- www/transwarp/db.py
+++ www/transwarp/db.py
@@ -476,7 +476,7 @@ def update(sql, *args):
 
 if __name__=='__main__':
     logging.basicConfig(level=logging.DEBUG)
-    create_engine('www-data', 'www-data', 'test')
+    create_engine('root', '', 'test')
     update('drop table if exists user')
     update('create table user (id int primary key, name text, email text, passwd text, last_modified real)')
     import doctest
